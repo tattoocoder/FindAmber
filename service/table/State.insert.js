d4b1a8a2a0fde3732267248827838089edbfb665
windowsazure <windowsazure>
2013-11-20 20:37:49 +0000

    Committing update from request /scmvfs/service/table/state.insert.js

--- service/table/State.insert.js
+++ service/table/State.insert.js
@@ -4,6 +4,7 @@ var req = require('request');
 function insert(item, user, request) {
 
     if (item.firstRun === true) {
+                console.log('first run ' + item.abbrev + ' getting data');
                 req.get({ url: item.url },function (error, result, body) {
                        if (error) {
                            console.error(error);
